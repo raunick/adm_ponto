97c269a54f3970bf85f81d4f115f2a4ee8648ad5
Raunick Vileforte <raunickbhdesign@gmail.com>
2024-01-04 16:33:38 -0300

    Update main.py
    correção de horas trabalhadas

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ def calcular_banco_de_horas(df_row):
     else:
         total_horas_trabalhadas = pd.to_timedelta(df_row['Horas Trabalhadas'])
         if pd.to_datetime(df_row['Data']).weekday() == 4:
-            jornada_diaria = pd.to_timedelta('9:00:00')
+            jornada_diaria = pd.to_timedelta('8:00:00')
         elif pd.to_datetime(df_row['Data']).weekday() == 5:
             jornada_diaria = pd.to_timedelta('00:00:00')
         elif pd.to_datetime(df_row['Data']).weekday() == 6:
@@ -83,7 +83,7 @@ def calcular_banco_de_horas(df_row):
         elif df_row['Dia da Semana'] == 'Feriado':
             jornada_diaria = pd.to_timedelta('00:00:00')
         else:
-            jornada_diaria = pd.to_timedelta('8:00:00')
+            jornada_diaria = pd.to_timedelta('9:00:00')
         banco_de_horas_timedelta = total_horas_trabalhadas - jornada_diaria
         horas = int(banco_de_horas_timedelta.total_seconds() // 3600)
         minutos = int((banco_de_horas_timedelta.total_seconds() % 3600) // 60)
